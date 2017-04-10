5c024c125c5f3c7c2f09c5b5158b51b849c3b918
wyw <wyw>
2017-04-10 18:57:47 +0800

    项目框架搭建完成

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+  //用户在请求当前这个index.php的时候
+  //使用的是如下的连接  http://www.studyit.com/index.php/模块名/文件名
+
+  //我们可以使用$_SERVER获取到用户在后面输入的模块名和文件名
+  //$pathinfo=$_SERVER["PATH_INFO"];
+
+  //这里给$pathinfo一个默认的值，默认的路径
+   $pathinfo="/dashboard/index";
+
+   //判断用户有没有在index.php后面跟上路径信息
+   if(array_key_exists("PATH_INFO",$_SERVER)){
+         //若有传进来的路径信息，就使用传进来把我们的默认值给覆盖掉
+          $pathinfo=$_SERVER["PATH_INFO"];
+   }
+
+  // echo $pathinfo
+  // echo substr($pathinfo,1);
+
+  //把获取到的路径信息，转换成数组
+  //数组中的第一个元素，就是用户传进来的模块名
+  //数组中第二个元素，就是用户传进来的文件名
+   $pathArr=explode("/",substr($pathinfo,1));
+
+   if(count($pathArr)==2){
+       //根据用户传进来的东西，动态的拼接文件路径，把文件include进来
+          include "/views/".$pathArr[0]."/".$pathArr[1].".html";
+   }else{
+         include "/views/dashboard/".$pathArr[0].".html";
+   }
+
+
+?>
